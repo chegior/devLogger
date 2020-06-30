651a48b6412927192274f66aeebf55f031bb88d3
chegior <chegiorca@hotmail.com>
2020-06-29 21:43:09 -0500

    Added typed data and use Interpolation

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./logs.component.css']
 })
 export class LogsComponent implements OnInit {
-
+  logs:{
+    id:string,
+    text:string,
+    date:any
+  }[];
   constructor() { }
 
   ngOnInit() {
+    this.logs =[
+      {id:'1', text:'Generated Component',date: new Date('12/26/2017 12:54:23')},
+      {id:'2', text:'Added bootstrap',date: new Date('12/26/2017 12:54:23')},
+      {id:'3', text:'Generated Second Component',date: new Date('12/26/2017 12:54:23')}
+    ]
+
   }
 
 }
